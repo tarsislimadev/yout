a6ce5511766ad8b6fb4dc18d3dcefc0cd3585a00
Tarsis <br.tmvdl@gmail.com>
2024-05-19 16:15:27 -0300

    Updating app files

--- src/app/public/utils/lists.js
+++ src/app/public/utils/lists.js
@@ -6,5 +6,5 @@ export const getEndpointsList = () => Array.from([
   new RequestModel('Yout APIs'),
   new RequestModel('News API (everything)', 'GET', 'https://newsapi.org/v2/everything', { query: ['from', 'to', 'language', 'sortBy', 'q', 'pageSize', 'page', 'apiKey'] }),
   new RequestModel('News API (top headlines)', 'GET', 'https://newsapi.org/v2/top-headlines', { query: ['country', 'category', 'q', 'pageSize', 'page', 'apiKey'] }),
-  new RequestModel('Voice RSS API (text to speech)', 'GET', 'http://api.voicerss.org/', { query: ['key', 'src', 'hl'] }, { content_type: 'audio/wav' }),
+  new RequestModel('Voice RSS API (text to speech)', 'GET', 'http://api.voicerss.org/', { query: ['src', 'hl', 'key'] }, { content_type: 'audio/wav' }),
 ])
