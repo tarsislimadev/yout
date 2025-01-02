fb4bc382fd6f552c1255bf23197718e216c97d71
Tarsis Lima <br.tmvdl@gmail.com>
2025-01-02 16:00:32 -0300

    Updating app files

--- generativelanguage.sh
+++ generativelanguage.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/sh
 
-. datetime.sh
+. ~/.datetime
 
-apiKey=""
+. ~/.generativelanguage_api_key
 
-generativelanguage=$( curl -sL -X POST "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${apiKey}" -H 'Content-Type: application/json' -d '{ "contents": [{ "parts":[{ "text": "Write a story about a magic backpack." }]}]}' )
+generativelanguage=$( curl -sL -X POST "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${generativelanguage_api_key}" -H 'Content-Type: application/json' -d '{ "contents": [{ "parts":[{ "text": "Write a story about a magic backpack." }]}]}' )
 
 echo "${generativelanguage}" > "generativelanguage.${datetime}.txt"
